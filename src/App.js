f0b4d4d996e72f726612279ad11f4ba715ace0e1
mahmoud khaled <mahmoodqasem543@gmail.com>
2022-10-04 22:49:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
-    </div>
+      <Hero/> 
+     </div>
   );
 }
 
